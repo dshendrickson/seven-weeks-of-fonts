5511e8d965c41c4833c2fa396331f28d0b77b874
Dale S Hendrickson <dale@dshendrickson.com>
2016-10-23 15:37:22 -0600

    CSS ids and variables set for weekthree

--- app/components/WeekFour.jsx
+++ app/components/WeekFour.jsx
@@ -7,13 +7,13 @@ export default class WeekFour extends Component {
     return (
       <div>
         <h3>Week Four</h3>
-          <p id="unna-one">
+          <p id="abril-fatface-one">
             “I love deadlines. I like the whooshing sound they make as they fly by.” ― Douglas Adams          </p>
-          <p id="lato-one">
+          <p id="open-sans-one">
             “I love deadlines. I like the whooshing sound they make as they fly by.” ― Douglas Adams          </p>
-          <p id="unna-two">
+          <p id="abril-fatface-two">
             “I love deadlines. I like the whooshing sound they make as they fly by.” ― Douglas Adams          </p>
-          <p id="lato-two">
+          <p id="open-sans-two">
             “I love deadlines. I like the whooshing sound they make as they fly by.” ― Douglas Adams          </p>
       </div>
     )
